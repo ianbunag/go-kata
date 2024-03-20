9f9e7201d9b76c83ae3b9ffbee280ccfd3c75640
Ian Buñag <jianbunag@gmail.com>
2024-03-20 17:45:06 +0800

    Complete kth largest element in a stream

--- leetcode/47_kth_largest_element_in_a_stream/47_kth_largest_element_in_a_stream_test.go
+++ leetcode/47_kth_largest_element_in_a_stream/47_kth_largest_element_in_a_stream_test.go
@@ -10,15 +10,10 @@ var _ = Describe("47KthLargestElementInAStream", func() {
 	It("it should return kth largest element in a stream", func() {
 		kthLargest := Constructor(3, []int{4, 5, 8, 2})
 
-		Skip("Skipping this test for now")
 		Expect(kthLargest.Add(3)).To(Equal(4))
-		Skip("Skipping this test for now")
 		Expect(kthLargest.Add(5)).To(Equal(5))
-		Skip("Skipping this test for now")
 		Expect(kthLargest.Add(10)).To(Equal(5))
-		Skip("Skipping this test for now")
 		Expect(kthLargest.Add(9)).To(Equal(8))
-		Skip("Skipping this test for now")
 		Expect(kthLargest.Add(4)).To(Equal(8))
 	})
 })
--- leetcode/47_kth_largest_element_in_a_stream/kth_largest_element_in_a_stream.go
+++ leetcode/47_kth_largest_element_in_a_stream/kth_largest_element_in_a_stream.go
@@ -1,15 +1,61 @@
 package kth_largest_element_in_a_stream
 
+import (
+	"container/heap"
+)
+
 type KthLargest struct {
+	minHeap []int
+	k       int
 }
 
+// Average time complexity: O(n log k)
+// Worst time complexity:   O(n log k)
+// Space complexity:        O(n)
 func Constructor(k int, nums []int) KthLargest {
-	return KthLargest{}
+	kthLargest := KthLargest{k: k}
+	heap.Init(&kthLargest)
+
+	for _, num := range nums {
+		kthLargest.Add(num)
+	}
+
+	return kthLargest
 }
 
-// Average time complexity: O(1)
-// Worst time complexity:   O(1)
-// Space complexity:        O(1)
+// Average time complexity: O(log n)
+// Worst time complexity:   O(log n)
+// Space complexity:        O(n)
 func (kthLargest *KthLargest) Add(val int) int {
-	return 0
+	heap.Push(kthLargest, val)
+
+	if kthLargest.Len() > kthLargest.k {
+		heap.Pop(kthLargest)
+	}
+
+	return kthLargest.minHeap[0]
+}
+
+func (kthLargest KthLargest) Len() int {
+	return len(kthLargest.minHeap)
+}
+
+func (kthLargest KthLargest) Less(i, j int) bool {
+	return kthLargest.minHeap[i] < kthLargest.minHeap[j]
+}
+
+func (kthLargest KthLargest) Swap(i, j int) {
+	kthLargest.minHeap[i], kthLargest.minHeap[j] = kthLargest.minHeap[j], kthLargest.minHeap[i]
+}
+
+func (kthLargest *KthLargest) Push(x interface{}) {
+	kthLargest.minHeap = append(kthLargest.minHeap, x.(int))
+}
+
+func (kthLargest *KthLargest) Pop() interface{} {
+	current := kthLargest.minHeap
+	value := (current)[len(current)-1]
+	kthLargest.minHeap = current[:len(current)-1]
+
+	return value
 }
